65e41bf376f20a78480d5f85077503de7ad34a02
Mahegwhatley <maheswarigwhatley@gmail.com>
2025-12-01 22:39:05 -0500

    Create variables.tf

---
+++ terraform/aws-eks-platform/terraform/aws-eks-platform/modules/eks/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS Cluster"
+  type        = string
+}
