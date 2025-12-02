81cb1ebaffd2ee07009b8fb250d0e4a18a60d1e1
Mahegwhatley <maheswarigwhatley@gmail.com>
2025-12-01 22:35:14 -0500

    Create main.tf

---
+++ terraform/aws-eks-platform/terraform/aws-eks-platform/modules/eks/main.tf
@@ -0,0 +1,43 @@
+# EKS Main Terraform File
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  manage_aws_auth_configmap = true
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+    }
+  }
+}
